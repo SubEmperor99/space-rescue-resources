d5533b7f7eaa64f174730a909579c88c5f733c29
SubEmperor99 <subscribeemporer@gmail.com>
2025-08-06 09:54:58 +1000

    update
    update

---
+++ Rooms/WelcomeScreen.py
@@ -0,0 +1,18 @@
+from GameFrame import Level
+from Objects.Title import Title        
+class WelcomeScreen(Level):
+    """
+    Intial screen for the game
+    """
+    def __init__(self, screen, joysticks):
+        Level.__init__(self, screen, joysticks)
+        # set background image
+        self.set_background_image("background.jpg")
+        # add title object
+        self.add_room_object(Title(self, 240, 200))        
+
+       # load sounds
+        self.bg_music = self.load_sound("Music.mp3")
+        
+        # play background music
+        self.bg_music.play(loops=1)        
